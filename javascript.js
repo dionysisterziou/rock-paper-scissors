8d7e70b2a86060d35516f82f15ac2b7661b4cc8c
Dionysis <dionysisterzios@gmail.com>
2022-07-11 14:26:21 +0300

    Configure computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,16 @@
 function computerPlay() {
-    alert('hello');
+    let number = Math.floor(Math.random() * 3) + 1;
+    let object;
+    
+    if (number === 1) {
+        object = 'Rock';
+    } else if (number === 2) {
+        object = 'Papper';
+    } else {
+        object = 'Scissors';
+    }
+
+    return object;
 }
 
 computerPlay();
