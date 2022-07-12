8830ff7cb5b5ce2ac1a3fef6e534b9083b921ade
Dionysis <dionysisterzios@gmail.com>
2022-07-12 13:27:18 +0300

    Configure playRound function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-    let number = Math.floor(Math.random() * 3) + 1;
+    const number = Math.floor(Math.random() * 3) + 1;
     let object;
     
     if (number === 1) {
@@ -13,4 +13,37 @@ function computerPlay() {
     return object;
 }
 
-computerPlay();
+const computerSelection = computerPlay();
+const playerSelection = 'Rock';
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Rock') {
+            return `Draw!`;
+        } else if (computerSelection === 'Papper') {
+            return `You loose!`;
+        } else {
+            return `You win!`;
+        }
+    } else if (playerSelection === 'Papper') {
+        if (computerSelection === 'Rock') {
+            return `You win!`;
+        } else if (computerSelection === 'Papper') {
+            return `Draw!`;
+        } else {
+            return `You loose!`;
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Rock') {
+            return `You loose!`;
+        } else if (computerSelection === 'Papper') {
+            return `You win`;
+        } else {
+            return `Draw!`;
+        }
+    } else {
+        return `You must select one of three: 'Rock', 'Papper' or 'Scissors'.`;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
