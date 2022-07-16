f473937a67afc9ebaed8caa78c58a24351bb60c2
Dionysis <dionysisterzios@gmail.com>
2022-07-16 22:12:25 +0300

    Add all functions inside game()

--- javascript.js
+++ javascript.js
@@ -20,50 +20,50 @@ function game() {
     
         return lowerCaseWord.replace(firstCharacter, firstCharacter.toUpperCase());
     }
+
+    function computerPlay() {
+        const number = Math.floor(Math.random() * 3) + 1;
+        let object;
+        
+        if (number === 1) {
+            object = 'Rock';
+        } else if (number === 2) {
+            object = 'Papper';
+        } else {
+            object = 'Scissors';
+        }
+    
+        return object;
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === 'Rock') {
+            if (computerSelection === 'Rock') {
+                return `Draw!`;
+            } else if (computerSelection === 'Papper') {
+                return `You loose!`;
+            } else {
+                return `You win!`;
+            }
+        } else if (playerSelection === 'Papper') {
+            if (computerSelection === 'Rock') {
+                return `You win!`;
+            } else if (computerSelection === 'Papper') {
+                return `Draw!`;
+            } else {
+                return `You loose!`;
+            }
+        } else if (playerSelection === 'Scissors') {
+            if (computerSelection === 'Rock') {
+                return `You loose!`;
+            } else if (computerSelection === 'Papper') {
+                return `You win`;
+            } else {
+                return `Draw!`;
+            }
+        }
+    }
 }
 
 game();
-
-function computerPlay() {
-    const number = Math.floor(Math.random() * 3) + 1;
-    let object;
-    
-    if (number === 1) {
-        object = 'Rock';
-    } else if (number === 2) {
-        object = 'Papper';
-    } else {
-        object = 'Scissors';
-    }
-
-    return object;
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock') {
-        if (computerSelection === 'Rock') {
-            return `Draw!`;
-        } else if (computerSelection === 'Papper') {
-            return `You loose!`;
-        } else {
-            return `You win!`;
-        }
-    } else if (playerSelection === 'Papper') {
-        if (computerSelection === 'Rock') {
-            return `You win!`;
-        } else if (computerSelection === 'Papper') {
-            return `Draw!`;
-        } else {
-            return `You loose!`;
-        }
-    } else if (playerSelection === 'Scissors') {
-        if (computerSelection === 'Rock') {
-            return `You loose!`;
-        } else if (computerSelection === 'Papper') {
-            return `You win`;
-        } else {
-            return `Draw!`;
-        }
-    }
-}
 
