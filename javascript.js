9f4b634faea1d429dbbb230613d455ef5b8352e1
Dionysis <dionysisterzios@gmail.com>
2022-07-17 20:24:26 +0300

    Report winner

--- javascript.js
+++ javascript.js
@@ -1,11 +1,60 @@
-function game() {
-    let computerWins = 0;
-    let playerWins = 0;
+let computerWins = 0;
+let playerWins = 0;
 
+function computerPlay() {
+    const number = Math.floor(Math.random() * 3) + 1;
+    let object;
+
+    if (number === 1) {
+        object = 'Rock';
+    } else if (number === 2) {
+        object = 'Papper';
+    } else {
+        object = 'Scissors';
+    }
+
+    return object;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Rock') {
+            return `Draw!`;
+        } else if (computerSelection === 'Papper') {
+            computerWins = countComputerWins(computerWins);
+            return `You loose!`;
+        } else {
+            playerWins = countPlayerWins(playerWins);
+            return `You win!`;
+        }
+    } else if (playerSelection === 'Papper') {
+        if (computerSelection === 'Rock') {
+            playerWins = countPlayerWins(playerWins);
+            return `You win!`;
+        } else if (computerSelection === 'Papper') {
+            return `Draw!`;
+        } else {
+            computerWins = countComputerWins(computerWins);
+            return `You loose!`;
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Rock') {
+            computerWins = countComputerWins(computerWins);
+            return `You loose!`;
+        } else if (computerSelection === 'Papper') {
+            playerWins = countPlayerWins(playerWins);
+            return `You win`;
+        } else {
+            return `Draw!`;
+        }
+    }
+}
+
+function game() {
     for (let round = 1; round <= 5; round++) {
         const playerSelection = capitalize(prompt("Rock, papper or scissors?"));
         const computerSelection = computerPlay();
-        
+
         if (playerSelection === null || playerSelection === "") {
             alert(`You must select between: "Rock", "Papper" or "Scissors".`);
             round--;
@@ -17,76 +66,32 @@ function game() {
         }
     }
 
-    console.log(computerWins);
-    console.log(playerWins);
-
-    function capitalize(word) {
-        let lowerCaseWord = word.toLowerCase();
-        let firstCharacter = lowerCaseWord.charAt(0);
-    
-        return lowerCaseWord.replace(firstCharacter, firstCharacter.toUpperCase());
+    if (computerWins > playerWins) {
+        console.log(`Computer wins!`);
+    } else if (computerWins < playerWins) {
+        console.log(`Player wins!`);
+    } else {
+        console.log(`It's a draw!`);
     }
+}
 
-    function computerPlay() {
-        const number = Math.floor(Math.random() * 3) + 1;
-        let object;
-        
-        if (number === 1) {
-            object = 'Rock';
-        } else if (number === 2) {
-            object = 'Papper';
-        } else {
-            object = 'Scissors';
-        }
-    
-        return object;
-    }
+function capitalize(word) {
+    let lowerCaseWord = word.toLowerCase();
+    let firstCharacter = lowerCaseWord.charAt(0);
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === 'Rock') {
-            if (computerSelection === 'Rock') {
-                return `Draw!`;
-            } else if (computerSelection === 'Papper') {
-                computerWins = countComputerWins(computerWins);
-                return `You loose!`;
-            } else {
-                playerWins = countPlayerWins(playerWins);
-                return `You win!`;
-            }
-        } else if (playerSelection === 'Papper') {
-            if (computerSelection === 'Rock') {
-                playerWins = countPlayerWins(playerWins);
-                return `You win!`;
-            } else if (computerSelection === 'Papper') {
-                return `Draw!`;
-            } else {
-                computerWins = countComputerWins(computerWins);
-                return `You loose!`;
-            }
-        } else if (playerSelection === 'Scissors') {
-            if (computerSelection === 'Rock') {
-                computerWins = countComputerWins(computerWins);
-                return `You loose!`;
-            } else if (computerSelection === 'Papper') {
-                playerWins = countPlayerWins(playerWins);
-                return `You win`;
-            } else {
-                return `Draw!`;
-            }
-        }
-    }
+    return lowerCaseWord.replace(firstCharacter, firstCharacter.toUpperCase());
+}
 
-    function countComputerWins(computerWins) {
-        computerWins = computerWins + 1;
+function countComputerWins(computerWins) {
+    computerWins = computerWins + 1;
 
-        return computerWins;
-    }
+    return computerWins;
+}
 
-    function countPlayerWins(playerWins) {
-        playerWins = playerWins + 1;
+function countPlayerWins(playerWins) {
+    playerWins = playerWins + 1;
 
-        return playerWins;
-    }
+    return playerWins;
 }
 
 game();
