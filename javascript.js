39cf97d699e6976522e0cd57c5a335bb9d8c612b
Dionysis <dionysisterzios@gmail.com>
2022-08-15 12:51:52 +0300

    Disable buttons when someone wins

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
+const score = document.querySelector('#score');
+const finalResult = document.querySelector('#finalResult');
 let computerWins = 0;
 let playerWins = 0;
+
+score.textContent = `${computerWins}-${playerWins}`;
 
 function computerPlay() {
     const number = Math.floor(Math.random() * 3) + 1;
@@ -17,49 +21,39 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const div = document.querySelector('#result');
-    const p = document.createElement('p');
+    const result = document.querySelector('#result');
 
     if (playerSelection === 'Rock') {
         if (computerSelection === 'Rock') {
-            p.textContent = `Draw!`;
-            div.appendChild(p);
+            result.textContent = `Draw!`;
         } else if (computerSelection === 'Papper') {
-            computerWins = countComputerWins(computerWins);
-            p.textContent = `You loose!`;
-            div.appendChild(p);
+            computerWins++;
+            result.textContent = `You loose!`;
         } else {
-            playerWins = countPlayerWins(playerWins);
-            p.textContent = `You win!`;
-            div.appendChild(p);
+            playerWins++;
+            result.textContent = `You win!`;
         }
     } else if (playerSelection === 'Papper') {
         if (computerSelection === 'Rock') {
-            playerWins = countPlayerWins(playerWins);
-            p.textContent = `You win!`;
-            div.appendChild(p);
+            playerWins++;
+            result.textContent = `You win!`;
         } else if (computerSelection === 'Papper') {
-            p.textContent = `Draw!`;
-            div.appendChild(p);
+            result.textContent = `Draw!`;
         } else {
-            computerWins = countComputerWins(computerWins);
-            p.textContent = `You loose!`;
-            div.appendChild(p);
+            computerWins++;
+            result.textContent = `You loose!`;
         }
     } else if (playerSelection === 'Scissors') {
         if (computerSelection === 'Rock') {
-            computerWins = countComputerWins(computerWins);
-            p.textContent = `You loose!`;
-            div.appendChild(p);
+            computerWins++;
+            result.textContent = `You loose!`;
         } else if (computerSelection === 'Papper') {
-            playerWins = countPlayerWins(playerWins);
-            p.textContent = `You win`;
-            div.appendChild(p);
+            playerWins++;
+            result.textContent = `You win`;
         } else {
-            p.textContent = `Draw!`;
-            div.appendChild(p);
+            result.textContent = `Draw!`;
         }
-    }
+    };
 }
 
 function game() {
@@ -71,17 +65,25 @@ function game() {
             const playerSelection = capitalize(button.id);
 
             playRound(playerSelection, computerSelection);
-        });
-    });
+            score.textContent = `${computerWins}-${playerWins}`;
 
-    // if (computerWins > playerWins) {
-    //     console.log(`Computer wins!`);
-    // } else if (computerWins < playerWins) {
-    //     console.log(`Player wins!`);
-    // } else {
-    //     console.log(`It's a draw!`);
-    // }
+            if (computerWins === 5) {
+                finalResult.textContent = 'Computer wins!';
+
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                })
+            } else if (playerWins === 5) {
+                finalResult.textContent = 'Player wins!';
+
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                })
+            }
+        });
+    });    
 }
+
 
 function capitalize(word) {
     let lowerCaseWord = word.toLowerCase();
@@ -90,17 +92,4 @@ function capitalize(word) {
     return lowerCaseWord.replace(firstCharacter, firstCharacter.toUpperCase());
 }
 
-function countComputerWins(computerWins) {
-    computerWins = computerWins + 1;
-
-    return computerWins;
-}
-
-function countPlayerWins(playerWins) {
-    playerWins = playerWins + 1;
-
-    return playerWins;
-}
-
 game();
-
